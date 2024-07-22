062f4c8ae8fb1af8db61ab5f07c169a000c8b5ce
prudhvirajboddu <prudhvirajboddu@gmail.com>
2024-07-22 15:17:38 -0400

    Trying to trigger

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "CAts")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Democratic")
+option_b = os.getenv('OPTION_B', "Republican")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
